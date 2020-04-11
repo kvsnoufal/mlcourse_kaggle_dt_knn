af49a655b4c306bea13fc2624306457f946053ad
root <root@machineX.05rlj5440lpenjw15kc5hsv13e.ax.internal.cloudapp.net>
2020-04-11 03:27:06 +0000

    init

---
+++ src/run.sh
@@ -0,0 +1,5 @@
+export KAGGLE_USERNAME=kvsnoufal
+export KAGGLE_KEY=e2ac13495fadc045443b79a59b883bdc
+
+# kaggle competitions submit -c ods-mlclass-dubai-2019-03-lecture3-hw -f submit_hyp_opt2.csv -m "Message"
+kaggle competitions submit -c ods-mlclass-dubai-2019-03-lecture3-hw -f rounded.csv -m "Message"
